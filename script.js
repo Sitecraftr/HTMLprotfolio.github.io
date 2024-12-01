52a7a75b9b7a622b0ed5300f92e42e7b509871d5
Sitecraftr <shasmithas2005@gmail.com>
2024-12-01 15:07:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu=document.quarySelector(".menu-links");
+    const icon=document.quarySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
